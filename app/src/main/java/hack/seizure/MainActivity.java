8020f58f3ed53055013fecd8aeb0fbe72824c9f9
akshay-nair <akshay.nair@programmer.net>
2016-01-23 05:46:15 -0500

    Android Studio set-up

---
+++ app/src/main/java/hack/seizure/MainActivity.java
@@ -0,0 +1,16 @@
+package hack.seizure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebase.client.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Firebase.setAndroidContext(this);
+  }
+}
